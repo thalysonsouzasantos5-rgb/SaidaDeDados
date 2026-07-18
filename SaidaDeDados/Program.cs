2d6dc117dcf48f23635db977bf44154dc4400c68
Thalyson <thalysonsouzasantos5@gmail.com>
2026-07-17 21:53:38 -0300

    Adicionar arquivos de projeto.

---
+++ SaidaDeDados/Program.cs
@@ -0,0 +1,11 @@
+﻿using static System.Console;
+
+int numero = 12;
+double flutuante = 2.85;
+string palavra = "SENAI";
+char caraceter = "s";
+bool logical1 = "true";
+bool logical2 = "false";
+
+WriteLine("Hello, World!");
+ReadKey();
